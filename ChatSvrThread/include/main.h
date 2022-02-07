2f94bcd6313ecfe0c8b66758cc857e8fc88b0c18
Hx <624496791@qq.com>
2022-02-07 20:55:17 +0800

    修改为半同步/半异步模式，因为半反应堆式的模型不适合tcp这种有状态服务

--- ChatSvrThread/include/main.h
+++ ChatSvrThread/include/main.h
@@ -7,23 +7,58 @@
 #include <cstdlib>
 #include <strings.h>
 #include <cassert>
+#include <functional>
+#include <map>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/epoll.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "threadpool.h"
 
 #define MAX_CONN_CNT 100
 #define MAX_EPOLL_CNT 5
 
-typedef int (*TaskFunc)(int, char *);
+#define BUFF_SIZE 20
 
 using namespace std;
 
-int addfd(int epoll_fd, int fd);
+typedef function<void()> lambdaType;
 
-int doRead();
+// 用户信息存储
+struct UserInfo
+{
+    int pipe[2];
+    int socket;
+
+    UserInfo()
+    {
+        reset();
+    }
+
+    void reset()
+    {
+        pipe[0] = -1;
+        pipe[1] = -1;
+        socket = -1;
+    }
+
+    bool isInit()
+    {
+        return socket == -1;
+    }
+};
+
+int addfd(int epoll_fd, int fd);
+int delfd(int epoll_fd, int fd);
+
+int doRead(int fd);
+int doSendAll(int read_fd);
+
+void doBroadCast(int read_fd);
+void doChildThread(int fd);
 
 void LOGD(const string str);
 void LOGE(const string str);
--- ChatSvrThread/src/main.cpp
+++ ChatSvrThread/src/main.cpp
@@ -1,16 +1,17 @@
 #include "../include/main.h"
-#include "../include/threadpool.h"
-#include <cassert>
-#include <netinet/in.h>
-#include <strings.h>
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <unistd.h>
+#include <functional>
 
-int conn_socks[MAX_CONN_CNT];
+// 广播线程和主线程的通讯
+int pipe_broadcast[2];
+// 用户信息存储
+map<int, UserInfo> userInfos;
+// 连接socket和userInfos中的索引映射
+map<int, int> sockfd_2_userIndex;
+int maxIndex = 0;
 int user_cnt = 0;
+
 bool stop_server = false;
-//ThreadPool<> g_thread_pool;
+ThreadPool<lambdaType> g_thread_pool;
 
 int main(int argc, char *argv[])
 {
@@ -44,6 +45,16 @@ int main(int argc, char *argv[])
         return 0;
     }
 
+    // 广播线程生成
+    pipe(pipe_broadcast);
+    auto func_broadcast = []{
+        doBroadCast(pipe_broadcast[0]);
+        return;
+    };
+    Task<function<void()>> task_broadcast(func_broadcast);
+    g_thread_pool.addTask(task_broadcast);
+
+    // epoll监听监听socket
     int epoll_fd = epoll_create(MAX_EPOLL_CNT);
     addfd(epoll_fd, fd);
 
@@ -57,7 +68,9 @@ int main(int argc, char *argv[])
         }
 
         for (int i = 0; i < ret_epoll; ++i) {
-            if (events[i].data.fd == fd && events[i].events & EPOLLIN)
+            int &event_fd = events[i].data.fd;
+            int &userIndex = sockfd_2_userIndex[event_fd];
+            if (event_fd == fd && events[i].events & EPOLLIN)
             {
                 // 收到connect请求
                 // 这里因为不需要用到客户端信息，所以直接用sockaddr类型
@@ -67,14 +80,41 @@ int main(int argc, char *argv[])
                 int new_fd = accept(fd, &addr_client, &sz_client);
 
                 // 增加连接socket监听
-                conn_socks[user_cnt] = new_fd;
-                addfd(epoll_fd, new_fd);
+                // 搜寻可用索引
+                int index = -1;
+                for (int j = 0; j <= maxIndex; ++j) {
+                    if (userInfos[j].isInit())
+                    {
+                        index = j;
+                        break;
+                    }
+                }
+                if (index == -1)
+                {
+                    index = ++maxIndex;
+                }
+
+                sockfd_2_userIndex[new_fd] = index;
+                userInfos[index].reset();
+                socketpair(AF_UNIX, SOCK_STREAM, 0, userInfos[index].pipe);
+                userInfos[index].socket = new_fd;
                 ++user_cnt;
+
+                auto func = [new_fd]{
+                    doChildThread(new_fd);
+                    return;
+                };
+                Task<function<void()>> task(func);
+                g_thread_pool.addTask(task);
             }
-            else 
+            else if (event_fd == userInfos[userIndex].pipe[0] && events[i].events & EPOLLIN)
             {
-                // 连接socket的事件
-                
+                // 收到工作线程的消息
+                char buff[BUFF_SIZE];
+                memset(buff, '\0', BUFF_SIZE);
+                int ret_byte = recv(event_fd, buff, BUFF_SIZE, 0);
+
+                send(pipe_broadcast[1], buff, ret_byte, 0);
             }
         }
         
@@ -87,12 +127,97 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+void doBroadCast(int read_fd)
+{
+    epoll_event events[MAX_EPOLL_CNT];
+    int epoll_fd = epoll_create(MAX_EPOLL_CNT);
+    addfd(epoll_fd, read_fd);
+
+    while (1) {
+        int num = epoll_wait(epoll_fd, events, MAX_EPOLL_CNT, -1);
+
+        if (num <= 0)
+        {
+            break;
+        }
+
+        for (int i = 0; i < num; ++i) {
+            if (events[i].data.fd == read_fd && events[i].events & EPOLLIN)
+            {
+
+                char buff[BUFF_SIZE];
+                memset(buff, '\0', BUFF_SIZE);
+                int ret_byte = recv(read_fd, buff, BUFF_SIZE, 0);
+
+                for (auto &elem : sockfd_2_userIndex) 
+                {
+                    UserInfo &info = userInfos[elem.second];
+                    // tee(read_fd, info.pipe[0], , unsigned int __flags);
+                    send(info.pipe[0], buff, ret_byte, 0);
+                }
+
+            }
+        }
+    }
+
+    close(epoll_fd);
+}
+
+void doChildThread(int fd, int pipe_fd[])
+{
+    // 子线程直接负责本fd的数据处理
+    epoll_event events[MAX_EPOLL_CNT];
+    int epoll_fd = epoll_create(MAX_EPOLL_CNT);
+
+    addfd(epoll_fd, fd);
+    addfd(epoll_fd, pipe_fd[1]);
+
+    while (1) {
+        int num = epoll_wait(epoll_fd, events, MAX_EPOLL_CNT, -1);
+
+        if (num <= 0)
+        {
+            break;
+        }
+
+        for (int i = 0; i < num; ++i) {
+            if (events[i].data.fd == fd && events[i].events & EPOLLIN)
+            {
+                char buff[BUFF_SIZE];
+                memset(buff, '\0', BUFF_SIZE);
+                int ret_byte = recv(fd, buff, BUFF_SIZE, 0);
+
+                send(pipe_fd[1], buff, ret_byte, 0);
+            }
+            else if (events[i].data.fd == pipe_fd[1] && events[i].events & EPOLLIN)
+            {
+                char buff[BUFF_SIZE];
+                memset(buff, '\0', BUFF_SIZE);
+                int ret_byte = recv(pipe_fd[1], buff, BUFF_SIZE, 0);
+
+                send(fd, buff, ret_byte, 0);
+            }
+        }
+    }
+
+    close(epoll_fd);
+}
+
 int addfd(int epoll_fd, int fd)
 {
     epoll_event event;
     event.data.fd = fd;
     event.events = EPOLLIN;
     epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &event);
+    return 0;
+}
+
+int delfd(int epoll_fd, int fd)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN;
+    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, &event);
     return 0;
 }
 
